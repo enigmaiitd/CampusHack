1f3cd7bd0197b7168f3c8c7b3d774aa6657afe1a
Vishal Singh <tmibvishal@gmail.com>
2019-02-11 00:05:19 +0530

    get partial information from layout to activity

--- app/src/main/java/iitd/enigma/libraryportal/AddNewBook_Activity.java
+++ app/src/main/java/iitd/enigma/libraryportal/AddNewBook_Activity.java
@@ -4,13 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class AddNewBook_Activity extends Activity {
+    EditText mBookName;
+    EditText mAccno;
+    EditText mIssuedTo;
+    EditText mDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adddialogbox);
+
 
         Button cancelButton_activityadb = (Button) findViewById(R.id.cancelButton_activityadb);
         Button addButton_activityadb = (Button) findViewById(R.id.addButton_activityadb);
@@ -18,7 +24,11 @@ public class AddNewBook_Activity extends Activity {
         addButton_activityadb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                UserBooksDB.BookInfo bookInfo = new UserBooksDB.BookInfo();
+                bookInfo.name = mBookName.getText().toString();
+                bookInfo.accessionNumber = mAccno.getText().toString();
+                bookInfo.issuedTo = mIssuedTo.getText().toString();
+
             }
         });
 
